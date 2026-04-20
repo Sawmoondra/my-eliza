3843b8f3326baca29d618c9245ebdeae1f92d3b2
Aryal, Samundra <samundraaryal@my.unt.edu>
2026-04-19 19:50:10 -0500

    Added comments and fixed errors in responseTracker

--- responseTracker.h
+++ responseTracker.h
@@ -4,23 +4,25 @@
 #include <string>
 #include <map>
 
+using namespace std;
+
 // ResponseTracker: tracks how many times the user has given the same input
 // so we can prompt them to give a different response if they repeat themselves.
 
 class ResponseTracker {
 public:
     // Record a user input; returns how many times it has been seen (including this time)
-    int recordInput(const std::string& input);
+    int recordInput(const string& input);
 
     // Returns true if the user has said this exact thing more than once
-    bool isRepeated(const std::string& input);
+    bool isRepeated(const string& input);
 
     // Returns a motivational nudge message for repeated inputs
-    std::string getNudgeMessage();
+    string getNudgeMessage();
 
 private:
     // Maps each user input string to the number of times it has been seen
-    std::map<std::string, int> inputHistory;
+    map<string, int> inputHistory;
 };
 
 #endif // RESPONSETRACKER_H
